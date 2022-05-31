1b1da1f2f001dc7935d033bae4ea6b521be6c90a
Charles Woods <charles.woods48@yahoo.com>
2022-05-31 15:51:33 -0500

    almost to problem 3

--- project/my-profile.js
+++ project/my-profile.js
@@ -1,7 +1,21 @@
 const addElement = () => {
     const myName = document.createElement('h1')
+    myName.className = 'name'
     myName.innerText = 'Charles Woods'
     document.body.appendChild(myName);
+
+    const details = document.createElement('ul')
+    details.className = 'my-details'
+    const detailArr = ['Hello', 'Goodbye', 'What up', 'Wowza']
+    
+    detailArr.map(ele => {
+        let li = document.createElement('li')
+        li.className = 'detail'
+        li.innerText = ele
+        details.appendChild(li)
+    })
+    
+    document.body.appendChild(details)
 }
 
 window.onload = addElement;
